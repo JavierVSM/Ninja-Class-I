7a1d3f5264309e6d5af1a0a5b1b3ee3ed4475952
JavierVSM <javier.vsm@gmail.com>
2021-11-23 15:06:17 -0600

    Commit

--- ninja.js
+++ ninja.js
@@ -1,13 +1,13 @@
 function ninja(name, health) {
     this.name = name;
     this.health = health;
-    this.speed=3;
-    this.strength=3;
+    var speed=3;
+    var strength=3;
     this.sayName = function(){
         console.log("My ninja name is " + this.name + "!");
     }
     this.showStats = function(){
-        console.log("Name: "+ this.name + ", Health: " + this.health + ", Speed: " + this.speed + ", Strength: " + this.strength);
+        console.log("Name: "+ this.name + ", Health: " + this.health + ", Speed: " + speed + ", Strength: " + strength);
     }
     this.drinkSake = function(){
       this.health = health+10;
